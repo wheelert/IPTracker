0de747cd7e3187f6aa942e074081aecf199360e9
Thomas Wheeler <wheelert@wheelerwire.com>
2021-06-14 09:39:03 -0700

    fixed install.sh for fresh installs

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@ APPDIR=/usr/share/IPTracker
 echo -e "\e[33m [ Checking for previous versions]\e[0m"
 if [ -d "$APPDIR" ]; then
     rm -rf $APPDIR
-    rm -rf $APPDIR/.config/IPTracker
+    rm -rf $HOME/.config/IPTracker
 fi
 
 
 
 echo -e "\e[32m [ Installing IPTracker ]\e[0m"
 mkdir $APPDIR
-mkdir $APPDIR/.config/IPTracker
+mkdir $HOME/.config/IPTracker
 cp *.py $APPDIR
 cp *.pyw $APPDIR
-cp data.sqlite $HOME/.config/IPTracker/
+#cp data.sqlite $HOME/.config/IPTracker/
 cp -R gui $APPDIR
 cp iptracker.png $APPDIR
 cp IPTracker.desktop /usr/share/applications/
